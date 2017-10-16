5fcedd6f05d824ac377b05de7972ef086ee6d6df
Psykotik <lambert.jord@gmail.com>
2017-10-16 13:09:50 +0200

    [Update] External token file
    Used external file for discord private token

--- bot.js
+++ bot.js
@@ -1,8 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var auth = require('./auth.json');
+
 
 client.on('ready', () => {
-  console.log('I am ready!');
+  console.log('Je suis pret!');
 });
 
 var prefix = ",";
@@ -13,7 +15,8 @@ client.on('message', message => {
   }
   else if (message.content.startsWith(prefix + 'ping')) {
         message.channel.sendMessage('Pong! Your ping is `' + `${Date.now() - message.createdTimestamp}` + ' ms`');
-    }
+  }
 });
 
-client.login('MzYzODEyOTIzNTMwODA1MjQ4.DLGqsw.aqo8-kR0Pd3qx9iG3OGdZnSR3zU');
+// External file for token + bot login. Should be the last line
+client.login(auth.token);
