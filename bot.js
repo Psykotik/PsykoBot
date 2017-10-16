aca8c6ad2c22b1ebabe338063c7ac9adce162516
Psykotik <lambert.jord@gmail.com>
2017-10-16 15:36:37 +0200

    [Add] GuildCreate and GuildDelete trigger
    Added trigger to log the bot when joining or leaving a guild.

--- bot.js
+++ bot.js
@@ -4,7 +4,18 @@ var auth = require('./auth.json');
 
 
 client.on('ready', () => {
-  console.log('Je suis pret!');
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+  client.user.setGame(`┬─┬﻿ ノ( ゜-゜ノ)`);
+});
+
+client.on("guildCreate", guild => {
+  // This event triggers when the bot joins a guild.
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+});
+
+client.on("guildDelete", guild => {
+  // this event triggers when the bot is removed from a guild.
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
 });
 
 var prefix = ",";
