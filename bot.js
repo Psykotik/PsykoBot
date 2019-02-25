ddbaf15919113a97eaa3cdad9240bed573f6cb63
Psykotik <Psykotik@users.noreply.github.com>
2019-02-26 00:16:23 +0100

    [Add] Uptime Function
    Added uptime in help command

--- bot.js
+++ bot.js
@@ -288,6 +288,14 @@ client.on('message', message => {
   //
   else if (message.content.startsWith(prefix + 'help')) {
     message.channel.send(lang.help);
+    let totalSeconds = (client.uptime / 1000);
+    let days = Math.floor(totalSeconds / 86400);
+    let hours = Math.floor(totalSeconds / 3600);
+    totalSeconds %= 3600;
+    let minutes = Math.floor(totalSeconds / 60);
+    let seconds = totalSeconds % 60;
+    let uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
+    message.channel.send(uptime);
   }
   //
   // Automatic response, nothing important here
