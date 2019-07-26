84dc344002181f208891355a83f187d3b15efed9
Psykotik <Psykotik@users.noreply.github.com>
2019-07-26 18:15:24 +0200

    ???

--- bot.js
+++ bot.js
@@ -75,89 +75,105 @@ client.on('message', message => {
     var Realm = argumentarray[1];
     var Region = argumentarray[2];
 
-    wow.ilvl2(Player,Realm,Region,4);
+    
+    var initializePromise = wow.ilvl2(Player, Realm, Region, 4);
+    initializePromise.then(function (result) {
+      userDetails = result;
+      var color;
 
-    var initializePromise = wow.ilvl2(Player,Realm,Region,4);
-    initializePromise.then(function(result) {
-        userDetails = result;
-        console.log("Initialized user details");
-        // Use user details from here
-        console.log(userDetails)
+      var initializePromiseColor = wow.getColor(userDetails.mythic_plus_scores.all);
+      initializePromiseColor.then(function (result) {
+        var color=result;
+      })
+      //console.log("Initialized user details");
+      // Use user details from here
+      //console.log(userDetails)
 
-        if (userDetails.hasOwnProperty('name')) {
-            var embed = new Discord.RichEmbed()
-                .setTitle("ilvl")
-                .setColor("#CD3333")
-                .setDescription("ilvl2 ")
-                //.setFooter(parsedJson.name + " global rank is " + parsedJson.globalrank, parsedJson.avatar)
-                .setThumbnail("http://51.77.157.113/Psykobot/information.png")
-                .setTimestamp()
-                //.setURL("https://apextab.com/" + parsedJson.aid)
-                .addField("toto")
-            message.channel.send(embed)
-            return embed;
+      if (userDetails.hasOwnProperty('name')) {
+        var embed = new Discord.RichEmbed()
+          .setTitle(userDetails.name + "'s informations (" + userDetails.race + " " + userDetails.active_spec_name + " " + userDetails.class + ")")
+          .setColor(color)
+          .addField("Raider.io score ", userDetails.mythic_plus_scores.all, false)
+          .addField("Raider.io overall Mythic+ rank", "World : " + userDetails.mythic_plus_ranks.overall.world + "\n Region : " + userDetails.mythic_plus_ranks.overall.region + " \nRealm : " + userDetails.mythic_plus_ranks.overall.realm, true)
+          .addField("Raider.io class Mythic+ rank", "World : " + userDetails.mythic_plus_ranks.class.world + "\n Region : " + userDetails.mythic_plus_ranks.class.region + "\n Realm : " + userDetails.mythic_plus_ranks.class.realm, true)
+          .addBlankField()
+          .addField(userDetails.mythic_plus_recent_runs[0].dungeon + " +" + userDetails.mythic_plus_recent_runs[0].mythic_level + " (+" + userDetails.mythic_plus_recent_runs[0].num_keystone_upgrades + ")",
+            "Score : " + userDetails.mythic_plus_recent_runs[0].score +
+            "\n Affixs :" + " [" + userDetails.mythic_plus_recent_runs[0].affixes[0].name + "](" + userDetails.mythic_plus_recent_runs[0].affixes[0].wowhead_url + ")" + ", [" + userDetails.mythic_plus_recent_runs[0].affixes[1].name + "](" + userDetails.mythic_plus_recent_runs[0].affixes[1].wowhead_url + ")" + ", [" + userDetails.mythic_plus_recent_runs[0].affixes[2].name + "](" + userDetails.mythic_plus_recent_runs[0].affixes[2].wowhead_url + ")" + ", [" + userDetails.mythic_plus_recent_runs[0].affixes[3].name + "](" + userDetails.mythic_plus_recent_runs[0].affixes[3].wowhead_url + ")\n", true)
+          
+            .addField(userDetails.mythic_plus_recent_runs[1].dungeon + " +" + userDetails.mythic_plus_recent_runs[1].mythic_level + " (+" + userDetails.mythic_plus_recent_runs[1].num_keystone_upgrades + ")",
+            "Score : " + userDetails.mythic_plus_recent_runs[1].score +
+            "\n Affixs :" + " [" + userDetails.mythic_plus_recent_runs[1].affixes[0].name + "](" + userDetails.mythic_plus_recent_runs[1].affixes[0].wowhead_url + ")" + ", [" + userDetails.mythic_plus_recent_runs[1].affixes[1].name + "](" + userDetails.mythic_plus_recent_runs[1].affixes[1].wowhead_url + ")" + ", [" + userDetails.mythic_plus_recent_runs[1].affixes[2].name + "](" + userDetails.mythic_plus_recent_runs[1].affixes[2].wowhead_url + ")" + ", [" + userDetails.mythic_plus_recent_runs[1].affixes[3].name + "](" + userDetails.mythic_plus_recent_runs[1].affixes[3].wowhead_url + ")\n", true)
+          .addField(userDetails.mythic_plus_recent_runs[2].dungeon + " +" + userDetails.mythic_plus_recent_runs[2].mythic_level + " (+" + userDetails.mythic_plus_recent_runs[2].num_keystone_upgrades + ")",
+            "Score : " + userDetails.mythic_plus_recent_runs[2].score +
+            "\n Affixs :" + " [" + userDetails.mythic_plus_recent_runs[2].affixes[0].name + "](" + userDetails.mythic_plus_recent_runs[2].affixes[0].wowhead_url + ")" + ", [" + userDetails.mythic_plus_recent_runs[2].affixes[1].name + "](" + userDetails.mythic_plus_recent_runs[2].affixes[1].wowhead_url + ")" + ", [" + userDetails.mythic_plus_recent_runs[2].affixes[2].name + "](" + userDetails.mythic_plus_recent_runs[2].affixes[2].wowhead_url + ")" + ", [" + userDetails.mythic_plus_recent_runs[2].affixes[3].name + "](" + userDetails.mythic_plus_recent_runs[2].affixes[3].wowhead_url + ")\n", true)
+          .setThumbnail(userDetails.thumbnail_url)
+          .setTimestamp()
+          .setURL(userDetails.profile_url)
+        message.channel.send(embed)
+        return embed;
 
-        }
+      }
 
-        message.channel.send("yoyo")
-        message.channel.send(userDetails)
-    }, function(err) {
-        console.log(err);
+      /* message.channel.send("yoyo")
+      message.channel.send(userDetails) */
+    }, function (err) {
+      console.log(err);
     })
 
     //message.channel.send(wow.ilvl2(Player,Realm,Region,4));
- /*
-    var apiLink = "https://raider.io/api/v1/characters/profile?region=" + Region + "&realm=" + Realm + "&name=" + Player + "&fields=gear"
+    /*
+       var apiLink = "https://raider.io/api/v1/characters/profile?region=" + Region + "&realm=" + Realm + "&name=" + Player + "&fields=gear"
+   
+       message.channel.send("Hey, i'm fetching the ilvl for " + Player + " on server " + Realm + " ( " + Region + " )"); // send arguments into message's channel
+   
+       tools.appendLogFile(tools.getTime() + " Fetching iLVL for " + Player + " on server " + Realm + " ( " + Region + " )");
+       //console.log(apiLink);
+   
+       request(apiLink, (error, response, body) => {
+         if (error) {
+           return console.error(error);
+         }
+         let json = body;
+         var parsedJson = JSON.parse(json);
+         //console.log(parsedJson);
+   
+   
+         if (parsedJson.hasOwnProperty('name')) {
+           message.channel.send({
+             embed: {
+               color: 3447003,
+               author: {
+                 name: parsedJson.name + " informations"
+               },
+               title: parsedJson.name + " " + "( " + parsedJson.race + " ) " + parsedJson.class + " " + parsedJson.active_spec_name + " " + parsedJson.faction + "" + "",
+               fields: [{
+                 name: "iLvl (equipped)",
+                 value: parsedJson.gear.item_level_equipped
+               },
+               {
+                 name: "iLvl (total)",
+                 value: parsedJson.gear.item_level_total
+               }
+               ],
+               timestamp: new Date(),
+               footer: {
+                 icon_url: client.user.avatarURL,
+                 text: "Psykobot ©"
+               }
+             }
+           });
+         } else {
+           message.channel.send("The player " + Player + " wasn't found. Please check syntax.")
+           message.channel.send("The correct syntax is :\n**" + prefix + "ilvl PLAYER-REALM-REGION** \nExample: **,ilvl Khìjazi-Illidan-EU**")
+         }
+   
+   
+   
+   
+       }) */
 
-    message.channel.send("Hey, i'm fetching the ilvl for " + Player + " on server " + Realm + " ( " + Region + " )"); // send arguments into message's channel
 
-    tools.appendLogFile(tools.getTime() + " Fetching iLVL for " + Player + " on server " + Realm + " ( " + Region + " )");
-    //console.log(apiLink);
-
-    request(apiLink, (error, response, body) => {
-      if (error) {
-        return console.error(error);
-      }
-      let json = body;
-      var parsedJson = JSON.parse(json);
-      //console.log(parsedJson);
-
-
-      if (parsedJson.hasOwnProperty('name')) {
-        message.channel.send({
-          embed: {
-            color: 3447003,
-            author: {
-              name: parsedJson.name + " informations"
-            },
-            title: parsedJson.name + " " + "( " + parsedJson.race + " ) " + parsedJson.class + " " + parsedJson.active_spec_name + " " + parsedJson.faction + "" + "",
-            fields: [{
-              name: "iLvl (equipped)",
-              value: parsedJson.gear.item_level_equipped
-            },
-            {
-              name: "iLvl (total)",
-              value: parsedJson.gear.item_level_total
-            }
-            ],
-            timestamp: new Date(),
-            footer: {
-              icon_url: client.user.avatarURL,
-              text: "Psykobot ©"
-            }
-          }
-        });
-      } else {
-        message.channel.send("The player " + Player + " wasn't found. Please check syntax.")
-        message.channel.send("The correct syntax is :\n**" + prefix + "ilvl PLAYER-REALM-REGION** \nExample: **,ilvl Khìjazi-Illidan-EU**")
-      }
-
-
-
-
-    }) */
-
-    
 
     /* message.channel.send({embed: {
       color: 3447003,
--- src/worldOfWarcraft.js
+++ src/worldOfWarcraft.js
@@ -55,38 +55,38 @@ module.exports = {
     ilvl2: function (Player, Realm, Region) {
 
         var options = {
-            url: "https://raider.io/api/v1/characters/profile?region=" + Region + "&realm=" + Realm + "&name=" + Player + "&fields=gear"
+            url: "https://raider.io/api/v1/characters/profile?region=" + Region + "&realm=" + Realm + "&name=" + Player + "&fields=gear,mythic_plus_scores_by_season,mythic_plus_ranks,mythic_plus_recent_runs,mythic_plus_scores"
         };
 
         //var apiLink = "https://raider.io/api/v1/characters/profile?region=" + Region + "&realm=" + Realm + "&name=" + Player + "&fields=gear"
         console.log(tools.getTime() + " Fetching iLVL for " + Player + " on server " + Realm + " ( " + Region + " )");
 
-        return new Promise(function(resolve, reject) {
+        return new Promise(function (resolve, reject) {
             // Do async job
-            request.get(options, function(err, resp, body) {
+            request.get(options, function (err, resp, body) {
                 if (err) {
                     reject(err);
                 } else {
                     resolve(JSON.parse(body));
                     let json = body;
-            var parsedJson = JSON.parse(json);
-            //console.log(parsedJson);
+                    var parsedJson = JSON.parse(json);
+                    //console.log(parsedJson);
 
 
-            if (parsedJson.hasOwnProperty('name')) {
-                var embed = new Discord.RichEmbed()
-                    .setTitle("ilvl")
-                    .setColor("#CD3333")
-                    .setDescription("ilvl2 ")
-                    //.setFooter(parsedJson.name + " global rank is " + parsedJson.globalrank, parsedJson.avatar)
-                    .setThumbnail("http://51.77.157.113/Psykobot/information.png")
-                    .setTimestamp()
-                    //.setURL("https://apextab.com/" + parsedJson.aid)
-                    .addField("toto")
+                    if (parsedJson.hasOwnProperty('name')) {
+                        var embed = new Discord.RichEmbed()
+                            .setTitle("ilvl")
+                            .setColor("#CD3333")
+                            .setDescription(parsedJson.achievement_points)
+                            //.setFooter(parsedJson.name + " global rank is " + parsedJson.globalrank, parsedJson.avatar)
+                            .setThumbnail("http://51.77.157.113/Psykobot/information.png")
+                            .setTimestamp()
+                            //.setURL("https://apextab.com/" + parsedJson.aid)
+                            .addField("toto")
 
-                return embed;
+                        return embed;
 
-            }
+                    }
                 }
             })
         })
@@ -126,7 +126,43 @@ module.exports = {
         }) */
 
     },
-    bar: function () {
-        // whatever
+    getColor: function (score) {
+
+        var apiLink = "https://raider.io/api/v1/mythic-plus/score-tiers";
+        var color = "#FFFFFF";
+
+        return new Promise(function (resolve, reject) {
+
+        
+
+        request(apiLink, (error, response, body) => {
+            if (error) {
+                return console.error(error);
+            }
+
+            if (response.statusCode == 200) {
+
+                let json = body;
+                var parsedJson = JSON.parse(json);
+
+                var scoreArray = [];
+
+                for (var i = 0; i < parsedJson.length; i++) {
+                    scoreArray.push(parsedJson[i].score);
+                }
+
+                for (var i = 0; i < scoreArray.length; i++) {
+                    var scoreInArray = scoreArray[i];
+                    var scoreInArrayNext = scoreArray[i + 1];
+
+                    if (score < scoreInArray && score > scoreInArrayNext) {
+                        color = parsedJson[i].rgbHex;
+                    }
+
+                }
+                return color;
+            }
+        })
+    })
     }
 };
